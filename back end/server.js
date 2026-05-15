5896973b51db1057aa838e1eb5e971e0d7be01df
Khushal429 <khushalrana2002@gmail.com>
2026-05-15 13:13:29 +0530

    Update server.js

--- back end/server.js
+++ back end/server.js
@@ -15,7 +15,7 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
-app.use("/api/payment", require("./routes/paymentRoutes"));
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
